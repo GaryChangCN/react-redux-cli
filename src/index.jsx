026ab5a67caaa284f57afc2591dc67557e51b885
zhangguanyu02 <zhangguanyu02@meituan.com>
2017-05-30 13:03:48 +0800

    add redux-promise

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore,applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import promise from 'redux-promise';
 import reducer from './reducer';
 
-const store = createStore(reducer,applyMiddleware(thunk));
+const store = createStore(reducer,applyMiddleware(thunk,promise));
 
 import View from './components/View.jsx';
 
